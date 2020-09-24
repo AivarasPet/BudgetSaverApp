02529661156c92ee351ede3a11a2edcae6205561
Kostas <kostascinskas@gmail.com>
2020-09-24 18:28:00 +0300

    Adding a MonthlyIncome class to calculate how many months remain untill desired goal will be met.

---
+++ MonthlyIncome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetSaverApp
+{
+    class MonthlyIncome
+    {
+        private int uGoal;
+        public int RemainingMonthsToWork(int salary, int budget, int goal)
+        {
+            int remainingMonths = 0;
+            uGoal = goal-budget;
+            if ((uGoal) > 0)
+            {
+                while (uGoal > 0)
+                {
+                    uGoal -= salary;
+                    remainingMonths++;
+                }
+            }
+            return remainingMonths;
+        }
+    }
+}
