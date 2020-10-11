78fef8e2da47605077574002bbf5765a64495145
Edvinas Kulionis <edvis.kulka@gmail.com>
2020-10-11 17:32:28 +0300

    Small changes to the Transaction class: -Removed unused function -Deleted unneeded comments -Rearranged constructor commands for readability

--- Transactions/Transaction.cs
+++ Transactions/Transaction.cs
@@ -4,29 +4,17 @@ namespace BudgetSaverApp.Transactions
 {
     public class Transaction
     {
-        public string TransactionType { get;}
+        public string TransactionType { get; }
         public float Amount { get; }
         public string Title { get; }
         public string Category { get; set; }
 
-
         public Transaction(string transactionType, float amount, string title, string category)
         {
-            
-
             this.TransactionType = transactionType;
-            this.Title = title;
             this.Amount = amount;
+            this.Title = title;
             this.Category = category;
         }
-
-        public void writeAmount()
-        {
-            Console.WriteLine(this.Amount);
-        }
-
-        //public abstract void AbstractMethod();   // Abstract method
-    
-        //public abstract int Y { get; }
     }
 }
