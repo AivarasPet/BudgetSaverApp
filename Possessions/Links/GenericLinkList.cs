e613f6694294339570454eed376f0db58d4e15bf
deividas.tilindis <deividas.tilindis@DESKTOP-QKUN3B3>
2020-11-03 21:15:35 +0200

    Added generic list for APILinks and ImageLinks, also reworked how API's and ImageLinks work, converted to JSON, Implemented GroupJoin

---
+++ Possessions/Links/GenericLinkList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetSaverApp.Possessions.Links
+{
+    class GenericLinkList<T>: ICollection<T>, IEnumerable<T>
+    {
+        ICollection<T> _items;
+
+
+        public GenericLinkList()
+        {
+            _items = new List<T>();
+        }
+
+        protected GenericLinkList(ICollection<T> collection)
+        {
+           
+            _items = collection;
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _items.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _items.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return _items.Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+}
--- Possessions/PossessionsService.cs
+++ Possessions/PossessionsService.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using BudgetSaverApp.Pricing;
 using System.IO;
-
-
+using BudgetSaverApp.Possessions.Links;
+using System.Linq;
 
 namespace BudgetSaverApp.Possessions
 {
@@ -28,15 +28,71 @@ namespace BudgetSaverApp.Possessions
             
             list.Clear();
             string json = File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + @"..\..\Data\Possessions.json");
+            
+
             if (json == null) return;
 
             list = JsonConvert.DeserializeObject<List<Possession>>(json);
+
+            string apiJson = File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + @"..\..\Data\APILinks.json");
+            string imageJson = File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + @"..\..\Data\ImageLinks.json");
+
+            GenericLinkList<ApiLink> apiList = JsonConvert.DeserializeObject<GenericLinkList<ApiLink>>(apiJson);
+            
+            GenericLinkList<ImageLink> imageList = JsonConvert.DeserializeObject<GenericLinkList<ImageLink>>(imageJson);
+
+
+            var completeList = list.GroupJoin(apiList, a => a.LinkOfAPI, b => b.id, (item, api) => new
+            {
+                Item = item,
+                Api = api
+            });
+
+            List<Possession> list1 = new List<Possession>();
+
+            foreach(var item in completeList)
+            {
+                Possession possession = item.Item;
+                possession.LinkOfAPI = item.Api.First().link;
+                list1.Add(possession);
+            }
+
+            var completeList2 = list.GroupJoin(imageList, a => a.LinkOfImage, b => b.id, (item, api) => new
+            {
+                Item = item,
+                Api = api
+            });
+
+            list = list1;
+
+            foreach (var item in list)
+            {
+                foreach(var itemImage in completeList2)
+                {
+                    if(item.LinkOfImage == itemImage.Item.LinkOfImage)
+                    {
+                        item.LinkOfImage = itemImage.Api.First().link;
+                    }
+                }
+            }
+
+            foreach (Possession possession in list)
+            {
+                Console.WriteLine(possession.Name);
+                Console.WriteLine(possession.LinkOfAPI);
+                Console.WriteLine(possession.LinkOfImage);
+            }
 
             foreach (Possession possession in list)
             {
                 if (possession == null) continue;
                 APIFetcher.AddDownloadEntity(possession.LinkOfAPI, (IApiCallback)possession);
             }
+
+            
+
+            
+
             APIFetcher.RunAllDownloadsAsync();
         }
     }
