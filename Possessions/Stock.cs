74b740ebc891a182ad682853a4954aa2af5b98b6
Kostas <kostascinskas@gmail.com>
2020-12-10 14:52:39 +0200

    1

--- Possessions/Stock.cs
+++ Possessions/Stock.cs
@@ -6,18 +6,20 @@ namespace BudgetSaverApp.Possessions
 {
     class Stock : Possession, IApiCallback
     {
+        Func<float, float, float> percentageChange = (valueInDollars, valueInDollarsBought) => (valueInDollars + valueInDollarsBought) / valueInDollarsBought * 100;
+
         public void OnAPIDownload(string APIData)
         {
             JObject jObject = JObject.Parse(APIData);
             var price = jObject["c"];
 
             this.ValueInDollars = Amount * float.Parse(price.ToString());
-            if (Compare<float>(this.ValueInDollarsWhenBought) == false) this.PercentageChangeInValue = percentageChange(this.ValueInDollars, this.ValueInDollarsWhenBought);
+            if (Compare(this.ValueInDollarsWhenBought) == false) this.PercentageChangeInValue = percentageChange(this.ValueInDollars, this.ValueInDollarsWhenBought);
             else this.PercentageChangeInValue = 0;
             Console.WriteLine(this.PercentageChangeInValue);
         }
 
-        Func<float, float, float> percentageChange = (valueInDollars, valueInDollarsBought) => (valueInDollars + valueInDollarsBought) / valueInDollarsBought * 100;
+        
         public bool Compare<T>(T x)
         {
             return EqualityComparer<T>.Default.Equals(x, default(T));
